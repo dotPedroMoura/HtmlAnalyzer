eb2577ed4ab1085ba9bf6d6717a0e5bbe213e4e3
dotPedroMoura <pedro.amqs@gmail.com>
2025-02-18 17:02:06 -0300

    wip

--- src/HtmlAnalyzer.java
+++ src/HtmlAnalyzer.java
@@ -1,15 +1,30 @@
 import java.io.BufferedReader;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.nio.CharBuffer;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HtmlAnalyzer {
     public static void main(String[] args) {
         try {
             String teste = GetHTML(URI.create(args[0]));
-            teste.stripIndent();
-            System.out.println(teste);
+            Pattern pattern = Pattern.compile("<(.*?)>", Pattern.DOTALL);
+            Matcher matcher = pattern.matcher(teste);
+            List<String> tags = new ArrayList<String>();
+            Iterator<String> iterator;
+
+            while (matcher.find()) {
+                tags.add(matcher.group(1));
+                iterator = tags.iterator();
+                if (tags.contains("html") && tags.contains("/html")) {
+                    System.out.println(tags.toString());
+                }
+            }
+
+//            System.out.println(teste);
         } catch (Exception e) {
             e.printStackTrace();
         }
